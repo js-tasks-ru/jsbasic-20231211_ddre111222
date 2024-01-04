2733acb96070a1778fce52adf89107cee77d3550
Denis Martynov <ddre111222@gmail.com>
2024-01-04 03:28:07 +0300

    3-module 1-5 tasks

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,11 @@
 function camelize(str) {
-  // ваш код...
+  return str
+    .split("-")
+    .map(function(item,index){
+      if (index == 0) {
+       return item;
+     } else {
+        return item[0].toUpperCase() + item.slice(1);
+      }
+    }).join("");
 }
--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,6 @@
-function showSalary(users, age) {
-  // ваш код...
+function showSalary(arr, numAge) {
+  return arr
+    .filter(({age}) => age <= numAge)
+    .map(arr => `${arr.name}, ${arr.balance}`)
+    .join("\n")
 }
